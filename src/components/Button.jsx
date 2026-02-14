fab2d549f1d3edc7f0cc92cd34d4aa48ddeddd67
Shubham <gharatshubham306@gmail.com>
2026-02-15 01:30:15 +0530

    Created Review and Footer sections

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-
-const Button = ({ label, iconURL, backgroundColor, textColor, borderColor }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
     <button
-      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
       ${
         backgroundColor
           ? `${backgroundColor} ${textColor} ${borderColor}`
-          : `bg-coral-red text-white border-coral-red`
-      }`}
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
     >
       {label}
 
       {iconURL && (
         <img
           src={iconURL}
-          alt="arrow right icon"
-          className="ml-2 w-5 h-5"
+          alt='arrow right icon'
+          className='ml-2 rounded-full bg-white w-5 h-5'
         />
       )}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
